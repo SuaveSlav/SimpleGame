02f8a0923f20f8f5337043355c67fd6a27c634ab
90305539 <90305539@hs-03223-macair.ad.edenpr.org>
2018-09-21 08:08:18 -0500

    Initial Commit

---
+++ SimpleGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleGame
+//
+//  Created by 90305539 on 9/21/18.
+//  Copyright © 2018 Emir Sahbegovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
